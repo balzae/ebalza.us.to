a97af804dcc62802e0b5ba34c54775fd1cdf8bb2
balzae <114008545+balzae@users.noreply.github.com>
2023-09-07 08:56:02 +0200

    test 17

---
+++ next/static/Td33b3pg4GvwnbQ6fgbV3/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-a58615004921c8c9.js"],"/404":["static/chunks/pages/404-3f259f553a761362.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
